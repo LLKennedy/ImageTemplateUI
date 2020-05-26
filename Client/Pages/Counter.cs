8471d612c38dc83c8593eee607d5b187a24aeab1
Luke Kennedy <luke.l.kennedy@gmail.com>
2020-05-26 22:28:29 +1000

    Render image from ImageTemplate

--- Client/Pages/Counter.cs
+++ Client/Pages/Counter.cs
@@ -1,4 +1,5 @@
 using System;
+using ImageTemplate;
 
 namespace ImageTemplateUI
 {
@@ -7,9 +8,22 @@ namespace ImageTemplateUI
         public class Counter
         {
             public int Count { get; set; }
+            public string ImageData { get; set; } = "";
             public Counter(int startCount = 0)
             {
                 Count = startCount;
+                var builder = new Builder();
+                using(var rendered = builder.Render(null))
+                {
+                    try
+                    {
+                        ImageData = rendered.B64();
+                    } 
+                    catch (Exception ex)
+                    {
+                        Console.Error.WriteLine("Error converting image to string: " + ex.Message);
+                    }
+                }
             }
 
             public void IncrementCount()
